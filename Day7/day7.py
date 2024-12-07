f57233aac6ac688e74752318bdb1af5dd8922322
Xascoria <59613709+Xascoria@users.noreply.github.com>
2024-12-07 13:09:28 +0800

    Day 7

---
+++ Day7/day7.py
@@ -0,0 +1,20 @@
+f = open("Day7\day7.txt", "r")
+
+c = f.read().strip()
+
+final = 0
+for i in c.split("\n"):
+    a,b=i.split(":")
+    a = int(a)
+    b = [int(i) for i in b.strip().split()]
+    c = {b[0]}
+
+    for j in range(1, len(b)):
+        r = set()
+        for k in c:
+            r |= {k * b[j], k + b[j], int(str(k) + str(b[j]))}
+        c = r
+    if a in c:
+        final += a
+
+print(final)
